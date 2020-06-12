91cb70d2f035e4b0174d45b5481ec1a6ec280eec
Jeffrey Rose <jtr219@outlook.com>
2020-06-11 22:37:46 -0400

    added morgan logger

--- api.js
+++ api.js
@@ -1,5 +1,12 @@
 import express from "express"
+import morgan from "morgan"
 
 const app = express()
+
+app.use(morgan("dev")) // log incoming reqs
+
+app.get("/api", (req, res) => {
+    res.end("insert API instructions here")
+})
 
 app.listen(3000, () => console.log("Listening on Port 3000"))
