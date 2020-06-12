1c2ff7c8c6cfe18e9ab8f4ca14d221916f0d7ff1
Jeffrey Rose <jtr219@outlook.com>
2020-06-11 22:54:30 -0400

    outline api routes

--- api.js
+++ api.js
@@ -5,8 +5,41 @@ const app = express()
 
 app.use(morgan("dev")) // log incoming reqs
 
+// Provide API instructions
 app.get("/api", (req, res) => {
     res.end("insert API instructions here")
+})
+/*
+const sudokuGrid = "sudokuGrid"
+
+// provide data on next recommended move
+app.get(`/api/solve-next/:${sudokuGrid}`, (req, res) => {
+
+})
+
+// provide data on all remaining steps
+app.get(`/api/solve-all/:${sudokuGrid}`, (req, res) => {
+    
+})
+
+// provide data on recommended next strategy
+app.get(`/api/strategy-next/:${sudokuGrid}`, (req, res) => {
+    
+})
+
+// provide date on all remaining strategies needed
+app.get(`/api/strategy-all/:${sudokuGrid}`, (req, res) => {
+    
+})
+
+// provide full data on puzzle, same as 'solve-all' route
+app.get(`/api/:${sudokuGrid}`, (req, res) => {
+    
+})
+*/
+// 404 - reject
+app.use((req, res) => {
+    res.status(404).end("404! No page found.")
 })
 
 app.listen(3000, () => console.log("Listening on Port 3000"))
